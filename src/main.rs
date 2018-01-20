23987ac432a0af86c816bf397c00fd309eb3a0ce
Juan Karlo Licudine <arebel-mac@macrebel.local>
2018-01-20 23:04:08 +0800

    Created the initial tests.

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,49 @@ use std::fs::File;
 use std::io::prelude::*;
 use csv::Reader;
 
+fn get_file_names(args: Vec<String>) -> (String, String) {
+    let src_file_name: String = args[1].to_owned();
+    let dest_file_name: String = {
+        let splitted: Vec<&str>;
+        if args.len() > 2 {
+            args[2].to_owned()
+        }
+        else {
+            splitted = src_file_name.split(".").collect();
+            let mut dest_name = splitted[0].to_string();
+            dest_name.push_str(".json");
+            dest_name.to_owned()
+        }
+    };
+    (src_file_name, dest_file_name)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_name: &String = &args[1];
-    println!("call path is {}", &args[0]);
-    println!("file_name is {}", file_name);
+    let (src_file_name, dest_file_name) = get_file_names(args);
+
+    println!("src_file_name: {}", src_file_name);
+    println!("dest_file_name: {}\n", dest_file_name);
     
-    let mut file:File = File::open(file_name)
+    let mut src_file:File = File::open(src_file_name)
         .expect("File not found");
 
     let mut contents: String = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Something went wrong reading the file.");
+    src_file.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
     
     let mut json = object!{};
     
     let mut rdr = Reader::from_reader(contents.as_bytes());
     let headers = rdr.headers()
         .expect("There was an error reading the headers.");
+    
     let mut records_iter = rdr.records();
 
     loop  {
         match records_iter.next() {
             Some(record) => {
                 let record: Vec<String> = record.unwrap();
-                println!("Records {:?}", record);
 
                 for index in 0..headers.len() {
                     let key: &str = &record[0];
@@ -48,9 +66,26 @@ fn main() {
             None => { break }
         }
     }
-    
 
-
-    println!("{}", json.to_string());
+    println!("Converted output:\n{}", json.to_string());
     
+    let mut dest_file:File = File::create(&dest_file_name)
+        .expect(&format!("Error creating the file: {}", dest_file_name)[..]);
+    dest_file.write_all(json.to_string().as_bytes())
+        .expect(&format!("Error writing to file: {}", dest_file_name)[..]);
+
+    println!("Successfully wrote to file {}", dest_file_name);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn file_names() {
+        let (src, dest) = super::get_file_names(vec!["path".to_string(), "csv.csv".to_string(), "csv.json".to_string()]);
+        assert_eq!(src, "csv.csv");
+        assert_eq!(dest, "csv.json");
+        let (src, dest) = super::get_file_names(vec!["path".to_string(), "csv.csv".to_string()]);
+        assert_eq!(src, "csv.csv");
+        assert_eq!(dest, "csv.json");
+    }
 }
